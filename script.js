7990e9ca0f261d25b5112a0560c58da3e6db4191
LemakKatyusha <lemak.kateryna@student.uzhnu.edu.ua>
2023-11-16 02:25:27 +0200

    1

---
+++ script.js
@@ -0,0 +1,12 @@
+let switchmode = document.getElementById("switchmode");
+
+switchmode.onclick = function (){
+    let theme  = document.getElementById("theme");
+    if(theme.getAttribute("href") == "lightmode.css"){
+        theme.href = "darkmode.css";
+
+    }
+    else{
+        theme.href = "lightmode.css";
+    }
+}
